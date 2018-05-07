5f03be311bf9b3b0616d5e9ff532e813d2a6f292
yogev <yogev.kr@gmail.com>
2018-05-07 11:47:27 +0300

    Run.py

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo "Starting the script"
+echo "--------"
+date
+PROJECT_PATH=PATH # Change to full path
+cd ${PROJECT_PATH}
+/usr/bin/python ${PROJECT_PATH}/main.py >> ${PROJECT_PATH}/log.txt
+echo "Finishing the script"
+echo "-------"
